301964b2f76f8f2a0509a31c8e729a4f2e3442e2
이동원 <104751794+movingone@users.noreply.github.com>
2022-11-13 00:28:38 +0900

    Update 6.java

--- 3/6.java
+++ 3/6.java
@@ -26,5 +26,30 @@ public class Main {
         result += (n - 1);
         System.out.println(result);
     }
-
 }
+========================================================================================
+/*
+Greedy 와 같이 최대한 으로 나누고 -1씩 빼면서 했어야 했는데 아직 그리디 사용이 익숙치 않았다
+그래서 냅다 내 생각대로 풀어버렸다. 다시 한번 핵심을 되짚어 보자면 최대한 큰수로 나누고 이후의 경우를 생각해 보자
+import java.util.Scanner;
+
+public class last1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int count = 0;
+
+        while (n != 1) {
+            if (n % k == 0) {
+                n /= k;
+                count++;
+            } else {
+                n -= 1;
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+*/
